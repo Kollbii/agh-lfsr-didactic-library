81e3dfd6958ccdae90b35aacb2f325d6a364e67d
Kollbii <lkollbek@gmail.com>
2022-06-19 17:48:39 +0200

    Whole project done

--- lib/pylfsr.py
+++ lib/pylfsr.py
@@ -316,7 +316,7 @@ class LFSR():
 	==================
 	'''
 
-	def __init__(self, fpoly=[5, 2], initstate='ones', verbose=False,counter_start_zero=True, nonlinear=False):
+	def __init__(self, fpoly=[5, 2], initstate='ones', verbose=False, counter_start_zero=True, nonlinear=False):
 		if isinstance(initstate, str):
 			if initstate == 'ones':
 				initstate = np.ones(np.max(fpoly))
@@ -350,26 +350,13 @@ class LFSR():
 
 		self.check()
 
-		self.SBOX1 =[
-					[0,1],
-					[0,1,2],
-					[0,1,2,3],
-					[0,1,3,4],
-					[1,3,4,5],
-					[1,2,4,5],
-					[2,4,5,7]]
-
-		self.SBOX2 =[
-    				[7, 0, 9, 5, 12, 6, 10, 3, 8, 11, 15, 2, 1, 13, 4, 14],
-    				[8, 3, 5, 9, 11, 12, 6, 10, 1, 13, 2, 14, 4, 7, 15, 0],
-    				[13, 6, 0, 10, 14, 3, 11, 5, 7, 1, 9, 4, 2, 8, 12, 15],
-    				[11, 2, 6, 13, 8, 14, 1, 4, 0, 5, 10, 3, 7, 9, 12, 15]]
+		
 
 	def info(self):
 		'''
 		Display the information about LFSR with current state of variables
 		'''
-		if self.nonlinear == True:
+		if self.nonlinear:
 			print('%d bit NLFSR with Non-linear function' % (self.M))
 		else:
 			print('%d bit LFSR with feedback polynomial %s' % (self.M, self.feedpoly))	
@@ -468,16 +455,30 @@ class LFSR():
 				self.seq  = np.append(self.seq, self.state[-1])
 
 		if self.nonlinear:
+			SBOX1 =[
+					[0,1],
+					[0,1,2],
+					[0,1,2,3],
+					[0,1,3,4],
+					[1,3,4,5],
+					[1,2,4,5],
+					[2,4,5,7]]
+
+			SBOX2 =[
+    				[7, 0, 9, 5, 12, 6, 10, 3, 8, 11, 15, 2, 1, 13, 4, 14],
+    				[8, 3, 5, 9, 11, 12, 6, 10, 1, 13, 2, 14, 4, 7, 15, 0],
+    				[13, 6, 0, 10, 14, 3, 11, 5, 7, 1, 9, 4, 2, 8, 12, 15],
+    				[11, 2, 6, 13, 8, 14, 1, 4, 0, 5, 10, 3, 7, 9, 12, 15]]
 			#TODO Make some nonlinear magic and produce feedbackbit out of given current state.
 
 			state_to_dec = int("".join(str(x) for x in self.state), 2)
-			bits = self.SBOX1[len(self.state) - 2]
+			bits = SBOX1[len(self.state) - 2]
 
 			for i in range(len(bits)):
 				index_col = int("".join(str(self.state[x]) for x in bits),2)%16
 				index_row = (bits[0] + bits[1] + bits[2] + bits[3])%4
 
-			sbox_value = self.SBOX2[index_row][index_col]
+			sbox_value = SBOX2[index_row][index_col]
 
 			b= sbox_value%2
 			
